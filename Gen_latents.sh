0903ca4aa2298b8f06ab0ba70a51bd842223e70c
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2025-06-29 17:01:36 +0800

    Update Gen_latents.sh

--- Gen_latents.sh
+++ Gen_latents.sh
@@ -3,8 +3,8 @@
 chmod +x cache_latents.py
 
 # Generate Latents
-python cache_latents.py --csv train.csv --out_dir ../workspace/thesis1/latents/train
+python cache_latents.py --csv train.csv --out_dir ~/thesis1/latents/train
 
-python cache_latents.py --csv dev.csv   --out_dir ../workspace/thesis1/latents/dev
+python cache_latents.py --csv dev.csv --out_dir ~/thesis1/latents/dev
 
-python cache_latents.py --csv test.csv   --out_dir ../workspace/thesis1/latents/test
+python cache_latents.py --csv test.csv --out_dir ~/thesis1/latents/test
