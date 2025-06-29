c30e377f4ff2448b5ba9903ed064ca302a07f479
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2025-06-29 22:58:09 +0800

    Add files via upload

---
+++ rwkv6_block_config_map.py
@@ -0,0 +1,164 @@
+from dataclasses import dataclass
+from typing import Optional
+from typing import Union
+import torch
+
+@dataclass
+class RWKV6BlockConfigMap:
+
+    """Configuration map for RWKV based models"""
+    # Key properties for the block / model
+    num_hidden_layers: int
+    hidden_size: int
+
+    head_size: int = 64
+    head_size_divisor: int = 8
+
+    # Dropout rate, should only be used in training
+    dropout_rate: float = 0.0
+
+    # Implementation backend to use
+    tmix_backend: str = "auto"
+
+    # ---
+    # OPTIONAL PROPS
+    #
+    # Optional properties which can be derived
+    # or can be overwritten by the user
+    # ---
+
+    # Channel mix / FFN block dimension size
+    hidden_size_ffn: Optional[int] = None
+    hidden_size_att: Optional[int] = None
+
+    # Current layer_id of the block
+    layer_id: Optional[int] = None
+
+    # number of heads
+    n_head: Optional[int] = None
+
+    # Device and Data type
+    device: Union[torch.device, str, None] = None
+    dtype: Union[torch.dtype, str, None] = None
+
+    # ---
+    # OPTIONAL PROPS FETCHER
+    # ---
+
+    def get_hidden_size_ffn(self) -> int:
+        '''
+        Returns the dimension of feed forward network
+        '''
+        if self.hidden_size_ffn is not None:
+            hidden_size_ffn = self.hidden_size_ffn
+        else:
+            hidden_size = self.hidden_size
+            assert hidden_size  % 32 == 0, f"hidden_size must be divisible by 32"
+            hidden_size_ffn = (self.hidden_size * 3.5) // 32 * 32
+
+        hidden_size_ffn = int(hidden_size_ffn)
+        assert hidden_size_ffn % 32 == 0, f"hidden_size_att must be divisible by 32"
+        return hidden_size_ffn
+    
+    def get_layer_id(self, fallback:int) -> int:
+        '''
+        Returns the layer id
+        '''
+        if self.layer_id is not None:
+            return self.layer_id
+        return fallback
+    
+    def get_device(self, fallback:str) -> torch.device:
+        '''
+        Returns the device
+        '''
+        if self.device is not None:
+            return torch.device(self.device)
+        if fallback is not None:
+            return torch.device(fallback)
+        return torch.get_default_device()
+    
+    def get_dtype(self, fallback:str) -> torch.dtype:
+        '''
+        Returns the dtype
+        '''
+        if self.dtype is not None:
+            key = self.dtype
+        else:
+            key = fallback
+
+        # if dtype is already torch.dtype
+        if isinstance(key, torch.dtype):
+            return key
+        
+        # Get and Check if the dtype is instance of torch.dtype
+        ret = getattr(torch, key) 
+        assert isinstance(ret, torch.dtype), f"Invalid dtype: {self.dtype}"
+        return ret
+    
+    def get_hidden_size_att(self) -> int:
+        '''
+        Returns the dimension of attention
+        '''
+        if self.hidden_size_att is not None:
+            hidden_size_att = self.hidden_size_att
+        else:
+            hidden_size = self.hidden_size
+            assert hidden_size  % 32 == 0, f"hidden_size must be divisible by 32"
+            hidden_size_att = hidden_size
+        assert hidden_size_att % 32 == 0, f"hidden_size_att must be divisible by 32 ({hidden_size_att})"
+        return hidden_size_att
+    
+    def get_n_head(self) -> int:
+        '''
+        Returns the number of heads
+        '''
+        if self.n_head is not None:
+            n_head = self.n_head
+        else:
+            hidden_size_att = self.get_hidden_size_att()
+            n_head = self.get_hidden_size_att() // self.head_size
+            assert hidden_size_att % n_head == 0 ,  f"hidden_size_att must be divisible by head_size ({self.head_size})"
+
+        return n_head
+
+    # ---
+    # Duplicator & Normalizer
+    # ---
+
+    def new_block_config_map(self, **kwargs) -> 'RWKV6BlockConfigMap':
+        '''
+        Returns a new config map with updated values
+        '''
+
+        new_dict = {}
+        for key in RWKV6BlockConfigMap.__dataclass_fields__:
+            if key in self.__dict__:
+                new_dict[key] = self.__dict__[key]
+        new_dict.update(kwargs)
+
+        return RWKV6BlockConfigMap(**new_dict)
+
+    @staticmethod
+    def normalize(config_map: any) -> 'RWKV6BlockConfigMap':
+        '''
+        Converts either maps, objs or RWKV6BlockConfigMap
+        '''
+        if isinstance(config_map, RWKV6BlockConfigMap):
+            return config_map
+        
+        dict_obj = None
+        if isinstance(config_map, dict):
+            dict_obj = config_map
+        elif hasattr(config_map, '__dict__'):
+            dict_obj = config_map.__dict__
+        
+        if dict_obj is not None:
+            # Filter for only valeus in RWKV6BlockConfigMap
+            new_dict = {}
+            for key, value in dict_obj.items():
+                if key in RWKV6BlockConfigMap.__dataclass_fields__:
+                    new_dict[key] = value
+            return RWKV6BlockConfigMap(**new_dict)
+
+        raise ValueError(f"Unsupported config_map type: {type(config_map)}")
---
+++ rwkv6_layer_block.py
@@ -0,0 +1,138 @@
+import torch
+from torch import nn
+from torch import Tensor
+from typing import Union
+from torch.nn import functional as F
+
+from .rwkv6_block_config_map import RWKV6BlockConfigMap
+from .rwkv6_channel_mix import RWKV6ChannelMix
+from .rwkv6_time_mix import RWKV6TimeMix
+
+class RWKV6LayerBlock(torch.nn.Module):
+    '''
+    layer block for RWKV V6
+    '''
+
+    def __init__(self, configMap: Union[RWKV6BlockConfigMap, any]):
+        super().__init__()
+
+        configMap:RWKV6BlockConfigMap = RWKV6BlockConfigMap.normalize(configMap)
+        self.configMap = configMap
+
+        # Get required props
+        hidden_size = configMap.hidden_size
+        layer_id = configMap.get_layer_id(-1)
+        device = configMap.get_device(None)
+        dtype = configMap.get_dtype('bfloat16')
+        dropout_rate = configMap.dropout_rate
+
+        # Validate the layer_id
+        assert layer_id >= 0, f'layer_id must be >= 0, got {layer_id}'
+
+        # Setup the layernorms, and mixes
+        self.ln1 = nn.LayerNorm(hidden_size, device=device, dtype=dtype)
+        self.ln2 = nn.LayerNorm(hidden_size, device=device, dtype=dtype)
+
+        if layer_id == 0:
+            self.ln0 = nn.LayerNorm(hidden_size, device=device, dtype=dtype)
+        else:
+            self.ln0 = nn.Identity(device=device)
+
+        # Setup the time and channel mix
+        self.att = RWKV6TimeMix(configMap)
+        self.ffn = RWKV6ChannelMix(configMap)
+
+        # Setup droupout at block level
+        if dropout_rate > 0.0:            
+            self.drop0 = nn.Dropout(p = dropout_rate,device=device)
+            self.drop1 = nn.Dropout(p = dropout_rate,device=device)
+        else:
+            self.drop0 = nn.Identity(device=device)
+            self.drop1 = nn.Identity(device=device)
+    
+    def forward(
+        self, x:torch.Tensor, 
+        last_state: tuple[torch.Tensor,torch.Tensor,torch.Tensor]
+        ) -> tuple[torch.Tensor,tuple[torch.Tensor,torch.Tensor,torch.Tensor]]:
+        '''
+        Forward the block given the input tokens and the last state
+        Last state is a tuple of the following
+        - time mix shift state
+        - time mix wkv state
+        - channel mix shift state
+
+        Returns a pair of the output embedding and the next state
+        '''
+
+        # # Ensure everything is in the right device
+        # x = x.to(self.ln1.weight.device)
+        # last_state = [ s.to(self.ln1.weight.device) for s in last_state ]
+
+        x = self.ln0(x)
+
+        # assert self.ln1(x) is not None
+        # assert last_state.tmix_shift is not None
+        # assert last_state.tmix_wkv is not None
+
+        att_out, tmix_shift, tmix_wkv = self.att(
+            self.ln1(x),
+            last_state[0], # tmix_shift,
+            last_state[1] # tmix_wkv
+        )
+
+        # x = x + att_out
+        x = self.drop0(x + att_out)
+
+        ffn_out, ffn_state = self.ffn(
+            self.ln2(x),
+            last_state[2] # cmix_shift,
+        )
+
+        # x = x + ffn_out
+        x = self.drop1(x + ffn_out)
+        
+        return x, (tmix_shift, tmix_wkv, ffn_state)
+    
+    @torch.compile(mode="default")
+    def forward_with_default_compile(
+        self, 
+        in_x:torch.Tensor, 
+        in_state: tuple[torch.Tensor,torch.Tensor,torch.Tensor],
+        out_x:torch.Tensor, 
+        out_state: tuple[torch.Tensor,torch.Tensor,torch.Tensor]
+        ) -> tuple[torch.Tensor,tuple[torch.Tensor,torch.Tensor,torch.Tensor]]:
+        '''
+        Compiled varient of the forward function
+        With no new tensors being created for the output
+        Useful for static memory allocation optimizations inference
+        '''
+        out_x[:], tmp_state = self.forward(in_x, in_state)
+        out_state[0][:], out_state[1][:], out_state[2][:] = tmp_state
+        return out_x, out_state
+
+    @torch.compile(mode="reduce-overhead")
+    def forward_with_reduce_compile(self, in_x: torch.Tensor, in_state: tuple[torch.Tensor,torch.Tensor,torch.Tensor]) -> tuple[torch.Tensor,torch.Tensor]:
+        '''
+        Compiled varient of the forward function
+        '''
+        return self.forward(in_x, in_state)
+    
+    def load_from_model_state_dict(self, model_state_dict:dict, layer_id:int=-1, non_blocking:bool=True):
+        '''
+        Given the Full/partial RWKV model weights, load the block weights accordingly
+        '''
+        if layer_id == -1:
+            layer_id = self.configMap.get_layer_id(1)
+            
+        # Get the current state_dict
+        current_state_dict = self.state_dict()
+
+        # Iterate each parameter in the state_dict, and compare from the model
+        for n in current_state_dict:
+            model_key = f"blocks.{layer_id}.{n}"
+            if model_key not in model_state_dict:
+                continue
+
+            # Copy the values from the state_dict
+            current_state_dict[n].copy_(model_state_dict[model_key], non_blocking=non_blocking)
+        
